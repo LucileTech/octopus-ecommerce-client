e7d08928eab35f56129294ad7892037734b6c8c5
Amélie Chabrand <amelie.chabrand@gmail.com>
2022-12-05 09:33:10 +0100

    creations effect focus

--- src/components/AllCreations/AllCreations.jsx
+++ src/components/AllCreations/AllCreations.jsx
@@ -24,12 +24,22 @@ const AllCreations = () => {
         {creations.map((element) => {
           return (
             <>
-              <Link to={`/creations/${element._id}`}>
+              <Link
+                className="creations-images-all-creations-container"
+                to={`/creations/${element._id}`}
+              >
                 <img
                   className="creations-images-all-creations"
                   src={element.img}
                   alt={element.title}
                 />
+                {/*<div className="creations-images-all-creations-focus-container">
+                  <img
+                    className="creations-images-all-creations-focus-image"
+                    src="https://res.cloudinary.com/dzkbycvev/image/upload/v1670182659/creation-details_ut1o7q.png"
+                    alt="focus-image"
+                  />
+          </div>*/}
               </Link>
               {/* <Link to={`${element._id}`}>{element.name}</Link> */}
               {/* <h3>{element.name}</h3>
